362146425ebf58bedd843b2c9ab159468e34533b
unknown <you@example.com>
2025-03-13 11:49:57 -0700

    task 2 hazirdir

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+
+
+class Program
+{
+    static int Faktorial(int num)
+    {
+        int hasil = 1;
+        for (int i = 1; i <= num; i++)
+        {
+            hasil *= i;
+        }
+        return hasil;
+    }
+
+    static void Main()
+    {
+        int num = 10;
+        int cavab = Faktorial(num);
+        Console.WriteLine(cavab);
+    }
+}
+
+
+
+
+
